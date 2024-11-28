f320c4ad58a91380ca2556c74331a38b265f7461
Ravi Kumar <149966502+ravikumar-94@users.noreply.github.com>
2024-11-29 02:45:36 +0530

    Delete Portfolio directory

--- Portfolio/script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".multiple-text",{
-    strings: ["Front-End Developer" , "Back-End Development"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
